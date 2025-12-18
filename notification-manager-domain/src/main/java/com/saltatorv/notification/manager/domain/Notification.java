491972d97520d923354e66c0c9bca651561531ef
Saltator <saltatorv@gmail.com>
2025-12-18 23:54:13 +0100

    [7]:feat - add NotificationId to Notification

--- notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/Notification.java
+++ notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/Notification.java
@@ -1,12 +1,15 @@
 package com.saltatorv.notification.manager.domain;
 
 class Notification {
+    private final NotificationId id;
     private final String message;
 
     private boolean isSent;
 
     public Notification(String message) {
+        this.id = NotificationId.generate();
         this.message = message;
+
         this.isSent = false;
     }
 
