a8f627065c80d9a11d0aaf99128d3580deef6e4d
Saltator <saltatorv@gmail.com>
2025-12-18 23:52:57 +0100

    [7]:feat - add NotificationId

---
+++ notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/NotificationId.java
@@ -0,0 +1,19 @@
+package com.saltatorv.notification.manager.domain;
+
+import java.util.UUID;
+
+class NotificationId {
+    private final String id;
+
+    private NotificationId() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public static NotificationId generate() {
+        return new NotificationId();
+    }
+
+    public String getId() {
+        return id;
+    }
+}
