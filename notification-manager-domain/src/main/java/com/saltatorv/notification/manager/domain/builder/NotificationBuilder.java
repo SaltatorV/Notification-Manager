baa83b598ab7512c3678b22036e0c7a12bd81a68
Saltator <saltatorv@gmail.com>
2025-12-21 00:20:54 +0100

    [7]:feat - implement NotificationBuilder

--- notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/builder/NotificationBuilder.java
+++ notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/builder/NotificationBuilder.java
@@ -1,12 +1,35 @@
 package com.saltatorv.notification.manager.domain.builder;
 
-class NotificationBuilder {
+import com.saltatorv.notification.manager.domain.builder.steps.email.EmailNotificationBuilder;
+import com.saltatorv.notification.manager.domain.builder.steps.email.EmailNotificationRecipientStep;
+import com.saltatorv.notification.manager.domain.builder.steps.shared.NotificationChannelStep;
+import com.saltatorv.notification.manager.domain.builder.steps.sms.SMSNotificationBuilder;
+import com.saltatorv.notification.manager.domain.builder.steps.sms.SMSNotificationRecipientStep;
+import com.saltatorv.notification.manager.domain.builder.steps.system.SystemNotificationBuilder;
+import com.saltatorv.notification.manager.domain.builder.steps.system.SystemNotificationRecipientStep;
 
-    private static NotificationBuilder build() {
-        return new NotificationBuilder();
-    }
+public class NotificationBuilder implements NotificationChannelStep {
 
     private NotificationBuilder() {
 
+    }
+
+    private static NotificationBuilder buildNotification() {
+        return new NotificationBuilder();
+    }
+
+    @Override
+    public SMSNotificationRecipientStep forSms() {
+        return new SMSNotificationBuilder();
+    }
+
+    @Override
+    public EmailNotificationRecipientStep forMail() {
+        return new EmailNotificationBuilder();
+    }
+
+    @Override
+    public SystemNotificationRecipientStep forSystem() {
+        return new SystemNotificationBuilder();
     }
 }
