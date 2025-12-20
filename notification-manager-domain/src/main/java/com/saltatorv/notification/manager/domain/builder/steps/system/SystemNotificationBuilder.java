9e0f1a1bf80401090a62d2b51f8f7c249c81d8df
Saltator <saltatorv@gmail.com>
2025-12-21 00:17:49 +0100

    [7]:feat - add SystemNotificationBuilder

---
+++ notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/builder/steps/system/SystemNotificationBuilder.java
@@ -0,0 +1,26 @@
+package com.saltatorv.notification.manager.domain.builder.steps.system;
+
+import com.saltatorv.notification.manager.domain.Notification;
+import com.saltatorv.notification.manager.domain.builder.steps.shared.NotificationFinalStep;
+
+public class SystemNotificationBuilder implements SystemNotificationRecipientStep, SystemNotificationMessageStep, NotificationFinalStep {
+    private String recipient;
+    private String message;
+
+    @Override
+    public SystemNotificationMessageStep recipient(String recipient) {
+        this.recipient = recipient;
+        return this;
+    }
+
+    @Override
+    public NotificationFinalStep message(String message) {
+        this.message = message;
+        return this;
+    }
+
+    @Override
+    public Notification create() {
+        return new Notification(this.message);
+    }
+}
