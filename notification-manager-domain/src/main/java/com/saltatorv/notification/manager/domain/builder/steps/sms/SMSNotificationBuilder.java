8756aacc13e2e3b9cf6f8023139c865e1706f38d
Saltator <saltatorv@gmail.com>
2025-12-21 00:15:17 +0100

    [7]:feat - add SMSNotificationBuilder

---
+++ notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/builder/steps/sms/SMSNotificationBuilder.java
@@ -0,0 +1,27 @@
+package com.saltatorv.notification.manager.domain.builder.steps.sms;
+
+import com.saltatorv.notification.manager.domain.Notification;
+import com.saltatorv.notification.manager.domain.builder.steps.shared.NotificationFinalStep;
+
+public class SMSNotificationBuilder implements SMSNotificationRecipientStep, SMSNotificationTextMessageStep, NotificationFinalStep {
+
+    private String recipient;
+    private String message;
+
+    @Override
+    public SMSNotificationTextMessageStep recipient(String recipient) {
+        this.recipient = recipient;
+        return this;
+    }
+
+    @Override
+    public NotificationFinalStep message(String message) {
+        this.message = message;
+        return this;
+    }
+
+    @Override
+    public Notification create() {
+        return new Notification(this.message);
+    }
+}
