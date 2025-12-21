85f3e4b0067ed0cddcca440cc3c02545ac76ec0c
Saltator <saltatorv@gmail.com>
2025-12-21 23:31:21 +0100

    [7]:feat - add SystemChannel

---
+++ notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/SystemChannel.java
@@ -0,0 +1,16 @@
+package com.saltatorv.notification.manager.domain;
+
+class SystemChannel extends Channel {
+    private final String recipient;
+    private final String message;
+
+    public SystemChannel(String recipient, String message) {
+        this.recipient = recipient;
+        this.message = message;
+    }
+
+    @Override
+    void send() {
+        System.out.println("Sending System notification to " + recipient + ": " + message);
+    }
+}
