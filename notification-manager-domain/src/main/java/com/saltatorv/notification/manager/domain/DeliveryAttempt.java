c59384b840bbe35523d531fb4b6a4463a2068a8b
Saltator <saltatorv@gmail.com>
2025-12-23 00:08:21 +0100

    [7]:refactor - extract direct values to constants

--- notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/DeliveryAttempt.java
+++ notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/DeliveryAttempt.java
@@ -1,22 +1,24 @@
 package com.saltatorv.notification.manager.domain;
 
 class DeliveryAttempt {
+    private static final int MIN_ATTEMPTS_TO_SEND = 1;
+    private static final int DEFAULT_ATTEMPTS_VALUE = 0;
+
     private final int maxAttemptsCounter;
     private int attempts;
 
     public DeliveryAttempt(int maxAttemptsCounter) {
-        if(maxAttemptsCounter < 1) {
+        if (maxAttemptsCounter < MIN_ATTEMPTS_TO_SEND) {
             throw new RuntimeException("Max attempts counter must be positive number");
         }
         this.maxAttemptsCounter = maxAttemptsCounter;
-        this.attempts = 0;
+        this.attempts = DEFAULT_ATTEMPTS_VALUE;
     }
 
     public void sendAttempt() {
-        if(hasRemainingSendAttempts()) {
-            this.attempts += 1;
-        }
-        else {
+        if (hasRemainingSendAttempts()) {
+            this.attempts++;
+        } else {
             throw new RuntimeException("Maximum number of attempts reached. Can not send this attempt.");
         }
     }
