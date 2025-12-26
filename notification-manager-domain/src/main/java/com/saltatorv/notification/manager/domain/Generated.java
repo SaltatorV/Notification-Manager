43d8d6d3e44dfb52915593a6cef0ac7054a6ba72
Saltator <saltatorv@gmail.com>
2025-12-26 10:17:28 +0100

    [NMB-19]:test - create Generated annotation to exclude methods, types and constructors from jacoco test coverage

---
+++ notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/Generated.java
@@ -0,0 +1,9 @@
+package com.saltatorv.notification.manager.domain;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE, ElementType.CONSTRUCTOR})
+public @interface Generated {
+}
