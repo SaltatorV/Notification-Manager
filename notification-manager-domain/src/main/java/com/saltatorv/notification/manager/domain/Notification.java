6f7cc451ee1fa77a746804ddeb9eefb743a430c1
Saltator <saltatorv@gmail.com>
2025-12-26 23:54:48 +0100

    [NMB-20]:feat - implement publishing and pulling domain events in Notification

--- notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/Notification.java
+++ notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/Notification.java
@@ -4,7 +4,10 @@ import com.saltatorv.notification.manager.domain.attempt.AttemptResult;
 import com.saltatorv.notification.manager.domain.attempt.Channel;
 import com.saltatorv.notification.manager.domain.attempt.DeliveryAttempt;
 import com.saltatorv.notification.manager.domain.events.DomainEvent;
+import com.saltatorv.notification.manager.domain.events.NotificationSendFailureEvent;
+import com.saltatorv.notification.manager.domain.events.NotificationSentEvent;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,18 +29,25 @@ public class Notification {
         return new Notification(channel, deliveryAttempt, new ArrayList<>());
     }
 
-    public static Notification restore(Channel channel, DeliveryAttempt deliveryAttempt) {
-        return new Notification(channel, deliveryAttempt, new ArrayList<>());
-    }
-
     public void send() {
         if (deliveryAttempt.hasRemainingSendAttempts()) {
             AttemptResult attemptResult = channel.send();
             deliveryAttempt.registerAttempt(attemptResult);
+            if (deliveryAttempt.wasSuccessful()) {
+                events.add(new NotificationSentEvent(id, Instant.now()));
+            } else {
+                events.add(new NotificationSendFailureEvent(id, Instant.now(), attemptResult.getExceptionMessage()));
+            }
         }
     }
 
     public boolean isSent() {
         return deliveryAttempt.wasSuccessful();
+    }
+
+    public List<DomainEvent> pullDomainEvents() {
+        List<DomainEvent> eventsToReturn = new ArrayList<>(events);
+        events.clear();
+        return eventsToReturn;
     }
 }
--- notification-manager-domain/src/test/java/com/saltatorv/notification/manager/domain/NotificationTest.java
+++ notification-manager-domain/src/test/java/com/saltatorv/notification/manager/domain/NotificationTest.java
@@ -1,6 +1,14 @@
 package com.saltatorv.notification.manager.domain;
 
+import com.saltatorv.notification.manager.domain.attempt.AttemptResult;
+import com.saltatorv.notification.manager.domain.attempt.Channel;
+import com.saltatorv.notification.manager.domain.attempt.DeliveryAttempt;
+import com.saltatorv.notification.manager.domain.events.DomainEvent;
+import com.saltatorv.notification.manager.domain.events.NotificationSendFailureEvent;
+import com.saltatorv.notification.manager.domain.events.NotificationSentEvent;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static com.saltatorv.notification.manager.domain.NotificationBuilder.buildNotification;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -9,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class NotificationTest {
 
     private Notification notification;
+    private List<DomainEvent> domainEvents;
 
     @Test
     public void testShouldSendSMSNotification() {
@@ -96,8 +105,51 @@ class NotificationTest {
         assertMessageIsNotSent();
     }
 
+    @Test
+    public void testShouldClearEventsAfterPullSuccessfulSentAttempt() {
+        // given
+        notification = buildNotification()
+                .forSms()
+                .recipient("+48123456789")
+                .message("Sample message")
+                .maxDeliveryAttempts(1)
+                .create();
+
+        sendNotification();
+
+        // when
+        pullEvents();
+
+        // then
+        assertDomainEventsAreClear();
+        assertPulledEventsContainsNotificationSentEvent();
+    }
+
+    @Test
+    public void testShouldClearEventsAfterPullFailedSentAttempt() {
+        // given
+        createNotificationWithFailingChannel();
+        sendNotification();
+        
+        // when 
+        pullEvents();
+
+        // then
+        assertDomainEventsAreClear();
+        assertPulledEventsContainsNotificationSendFailureEvent();
+    }
+
+    private void createNotificationWithFailingChannel() {
+        notification = Notification.create(new FailingChannel(), new DeliveryAttempt(2));
+    }
+
+
     private void sendNotification() {
         notification.send();
+    }
+
+    private void pullEvents() {
+        domainEvents = notification.pullDomainEvents();
     }
 
     private void assertMessageIsSent() {
@@ -108,5 +160,24 @@ class NotificationTest {
         assertFalse(notification.isSent());
     }
 
+    private void assertDomainEventsAreClear() {
+        assertTrue(notification.pullDomainEvents().isEmpty());
+    }
+
+    private void assertPulledEventsContainsNotificationSentEvent() {
+        assertTrue(domainEvents.stream().anyMatch(NotificationSentEvent.class::isInstance));
+    }
+    
+    private void assertPulledEventsContainsNotificationSendFailureEvent() {
+        assertTrue(domainEvents.stream().anyMatch(NotificationSendFailureEvent.class::isInstance));
+    }
+
+    private class FailingChannel extends Channel {
+
+        @Override
+        public AttemptResult send() {
+            return AttemptResult.createForFailure("Fail");
+        }
+    }
 
 }
