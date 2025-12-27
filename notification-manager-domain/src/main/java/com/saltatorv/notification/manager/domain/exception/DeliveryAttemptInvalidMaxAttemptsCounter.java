a43f7206b9b5892c3ae91a2d8d792ccc9056bd66
Saltator <saltatorv@gmail.com>
2025-12-28 00:08:17 +0100

    [NMB-21]:feat - default constructors from domain exceptions

--- notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/exception/DeliveryAttemptInvalidMaxAttemptsCounter.java
+++ notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/exception/DeliveryAttemptInvalidMaxAttemptsCounter.java
@@ -2,4 +2,12 @@ package com.saltatorv.notification.manager.domain.exception;
 
 public class DeliveryAttemptInvalidMaxAttemptsCounter extends DomainException {
     private final static String MESSAGE = "Max attempts counter must be positive number";
+
+    public DeliveryAttemptInvalidMaxAttemptsCounter() {
+        super(MESSAGE);
+    }
+
+    public DeliveryAttemptInvalidMaxAttemptsCounter(Throwable cause) {
+        super(MESSAGE, cause);
+    }
 }
--- notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/exception/DeliveryAttemptLimitExceededException.java
+++ notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/exception/DeliveryAttemptLimitExceededException.java
@@ -3,4 +3,11 @@ package com.saltatorv.notification.manager.domain.exception;
 public class DeliveryAttemptLimitExceededException extends DomainException {
     private final static String MESSAGE = "Maximum number of attempts reached. Can not send this attempt.";
 
+    public DeliveryAttemptLimitExceededException() {
+        super(MESSAGE);
+    }
+
+    public DeliveryAttemptLimitExceededException(Throwable cause) {
+        super(MESSAGE, cause);
+    }
 }
