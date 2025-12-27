e17527960e80188d9767aa23b59069cf1608f50f
Saltator <saltatorv@gmail.com>
2025-12-27 15:32:50 +0100

    [NMB-21]:feat - remove getReason from DomainException

--- notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/exception/DeliveryAttemptInvalidMaxAttemptsCounter.java
+++ notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/exception/DeliveryAttemptInvalidMaxAttemptsCounter.java
@@ -2,9 +2,4 @@ package com.saltatorv.notification.manager.domain.exception;
 
 public class DeliveryAttemptInvalidMaxAttemptsCounter extends DomainException {
     private final static String MESSAGE = "Max attempts counter must be positive number";
-
-    @Override
-    public String getReason() {
-        return MESSAGE;
-    }
 }
--- notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/exception/DeliveryAttemptLimitExceededException.java
+++ notification-manager-domain/src/main/java/com/saltatorv/notification/manager/domain/exception/DeliveryAttemptLimitExceededException.java
@@ -1,13 +1,6 @@
 package com.saltatorv.notification.manager.domain.exception;
 
-import com.saltatorv.notification.manager.domain.Generated;
-
 public class DeliveryAttemptLimitExceededException extends DomainException {
-  private final static String MESSAGE = "Maximum number of attempts reached. Can not send this attempt.";
+    private final static String MESSAGE = "Maximum number of attempts reached. Can not send this attempt.";
 
-  @Generated
-  @Override
-  public String getReason() {
-    return MESSAGE;
-  }
 }
